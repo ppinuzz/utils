29852de752527d52327586cda34f008d2fe2c774
Andrea Pinardi <andreapinardi319@gmail.com>
2026-08-17 18:22:19 +0200

    Update type annotations

--- utils/cleanbib.py
+++ utils/cleanbib.py
@@ -11,15 +11,15 @@ import argparse
 
 #%% FUNCTIONS
 
-def clean_bibfile(bib_file: Path | str, clean_bib_file: Path | str, verbose: bool = True):
+def clean_bibfile(bib_file: Path | str, clean_bib_file: Path | str, verbose: bool = True) -> None:
     """
-    Clean BibTeX .bib file, removing double braces in from title field
+    Clean BibTeX .bib file, removing double braces in from title field.
 
     Parameters
     ----------
-    bib_file : Path | str
+    bib_file : Path or str
         Bibliography file.
-    clean_bib_file : Path | str
+    clean_bib_file : Path or str
         Output file where "cleaned" bib file is written.
     verbose : bool, optional
         Whether to print user messages. The default is True.
@@ -61,7 +61,7 @@ def clean_bibfile(bib_file: Path | str, clean_bib_file: Path | str, verbose: boo
 # parse_args() takes a list of arguments to parse and parses them into an 
 # argparse.Namespace object whose attributes are accessible using dot notation 
 # => you can pass the arguments
-#   - either manually as a list of strings
+#   - either manually as a list of strings, e.g.
 #       args = parser.parse_args(['myinputfile.txt', 'myoutput.txt'])
 #   - or by reading them from sys.argv[1:] (ignoring 0th element, as that is
 #   always the name of the command) => default option
@@ -69,15 +69,15 @@ def clean_bibfile(bib_file: Path | str, clean_bib_file: Path | str, verbose: boo
 # by including argv=None in the argument list I can:
 #   - use the CLI as a regular CLI when called from the terminal
 #   - call the CLI from within a Python script by explicitly passing the 
-#   list of input arguments I'd use if I called it from the terminal
+#   list of input arguments I'd use if I called it from the terminal, e.g.
 #       read_cli_args(['myinputfile.txt', 'myoutput.txt'])
-def read_cli_args(argv=None):
+def read_cli_args(argv: list[str] | None = None) -> argparse.Namespace:
     """
     Parse command-line arguments.
 
     Parameters
     ----------
-    argv : list of str, optional
+    argv : list[str] or None, optional
         List of input arguments to parse. If None, they will be read from 
         ``sys.argv[1:]``. The default is None.
 
@@ -117,7 +117,19 @@ def read_cli_args(argv=None):
 #%% APPLICATION ENTRY POINT
 
 # same as above for the argv=None argument
-def main(argv=None):
+def main(argv: list[str] | None = None) -> None:
+    """
+    Application entry point.
+
+    Parameters
+    ----------
+    argv : list[str] or None, optional
+        List of arguments passed to the command line utility. If None, they are
+        read from the CLI; if not None, arguments and flags can be passed explicitly.
+        The default is None.
+
+    """
+    
     args = read_cli_args(argv)
     bibfile = Path(args.bibfile)
     # keep the same parent directory as bibfile, and use a user-defined name
